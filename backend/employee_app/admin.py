ce2665db3b50487825e17b34143d25cc3744ab90
Yashoza19 <yoyashoza@gmail.com>
2021-03-05 01:12:54 -0500

    Creating Django app with MySQL

---
+++ backend/employee_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from . import *
+
+admin.site.register(models.Employees)
+admin.site.register(models.Salaries)
+''' admin.site.register(models.Departments)
+admin.site.register(models.DeptEmp)
+admin.site.register(models.DeptManager)
+admin.site.register(models.Titles) '''
